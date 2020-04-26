0f4f3ebc69741d0778948b4b7ab228ebf3f6e014
yuhuakai <750665849@qq.com>
2020-04-27 04:20:40 +0800

    two

---
+++ precache-manifest.448a177faf20e492b0b272922cf756b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd404559234eb9c4069ede2897eaa071",
+    "url": "/react-sunmedia/index.html"
+  },
+  {
+    "revision": "686676f0eeb3b448b202",
+    "url": "/react-sunmedia/static/css/main.befa9a2d.chunk.css"
+  },
+  {
+    "revision": "7800a380179a5c254b21",
+    "url": "/react-sunmedia/static/js/2.12639ebd.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-sunmedia/static/js/2.12639ebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686676f0eeb3b448b202",
+    "url": "/react-sunmedia/static/js/main.278aa7c2.chunk.js"
+  },
+  {
+    "revision": "8dc62f9525856f6fef64",
+    "url": "/react-sunmedia/static/js/runtime-main.bdcbaefe.js"
+  },
+  {
+    "revision": "64523657d9d79a72132b928d096940dc",
+    "url": "/react-sunmedia/static/media/u2_normal.64523657.gif"
+  }
+]);
